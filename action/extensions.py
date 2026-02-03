002300274604eafc8087d30939af7cd0e6b9d5e2
bulricht <108867473+bulricht@users.noreply.github.com>
2026-02-03 14:52:11 +0100

    improve formatting

--- action/extensions.py
+++ action/extensions.py
@@ -5,16 +5,16 @@ import pandas as pd
 from py_data_rules.data_type import DataType
 
 
-class EMOBONRange(DataType):
-    @staticmethod
-    def match(instance):
-        assert instance
-        try:
-            a, b = map(str.strip, instance.split("-"))
-            assert float(a) < float(b)
-            return True
-        except (ValueError, AssertionError):
-            return False
+def read_emobon_csv(path):
+    df = (
+        pd.read_csv(path, dtype=object, keep_default_na=False)
+        .astype(str)
+        .map(lambda _: _.strip())
+        .map(lambda _: "" if _ == "nan" else _)
+        .map(lambda _: "" if (_ == "NA") or (_ =="ΝΑ") else _)  # prevent unicode confusion by replacing both greek and latin NA)
+    )
+    df = df[~(df == "").all(axis=1)]  # drop empty rows
+    return df
 
 
 class EMOBONList(DataType):
@@ -27,13 +27,13 @@ class EMOBONList(DataType):
             return True
 
 
-def read_emobon_csv(path):
-    df = (
-        pd.read_csv(path, dtype=object, keep_default_na=False)
-        .astype(str)
-        .map(lambda _: _.strip())
-        .map(lambda _: "" if _ == "nan" else _)
-        .map(lambda _: "" if (_ == "NA") or (_ =="ΝΑ") else _)  # prevent unicode confusion by replacing both greek and latin NA)
-    )
-    df = df[~(df == "").all(axis=1)]  # drop empty rows
-    return df
+class EMOBONRange(DataType):
+    @staticmethod
+    def match(instance):
+        assert instance
+        try:
+            a, b = map(str.strip, instance.split("-"))
+            assert float(a) < float(b)
+            return True
+        except (ValueError, AssertionError):
+            return False
